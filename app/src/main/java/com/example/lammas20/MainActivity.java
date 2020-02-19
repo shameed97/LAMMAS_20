e887ea221506b203083fdbbbf3edcbd465099828
shameed97 <shameed97.pno@gmail.com>
2020-02-19 11:55:49 +0530

    Project finished for College Culturals

--- app/src/main/java/com/example/lammas20/MainActivity.java
+++ app/src/main/java/com/example/lammas20/MainActivity.java
@@ -5,48 +5,54 @@ import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME = 3000;
-    private ImageView imageView;
+    private static int SPLASH_TIME = 2000;
+    private RelativeLayout relativeLayout;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView=findViewById(R.id.image_splash);
+        relativeLayout = findViewById(R.id.relative);
         setAnimation();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+
+                        startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
 
             }
         }, SPLASH_TIME);
     }
 
     private void setAnimation() {
-        ObjectAnimator scaleXAnimation = ObjectAnimator.ofFloat(findViewById(R.id.image_splash), "scaleX", 5.0F, 1.0F);
+        ObjectAnimator scaleXAnimation = ObjectAnimator.ofFloat(findViewById(R.id.relative), "scaleX", 5.0F, 1.0F);
         scaleXAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
         scaleXAnimation.setDuration(2000);
-        ObjectAnimator scaleYAnimation = ObjectAnimator.ofFloat(findViewById(R.id.image_splash), "scaleY", 5.0F, 1.0F);
+        ObjectAnimator scaleYAnimation = ObjectAnimator.ofFloat(findViewById(R.id.relative), "scaleY", 5.0F, 1.0F);
         scaleYAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
         scaleYAnimation.setDuration(2000);
-        ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(findViewById(R.id.image_splash), "alpha", 0.0F, 1.0F);
+        ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(findViewById(R.id.relative), "alpha", 0.0F, 1.0F);
         alphaAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
         alphaAnimation.setDuration(2000);
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.play(scaleXAnimation).with(scaleYAnimation).with(alphaAnimation);
         animatorSet.start();
-        findViewById(R.id.image_splash).setAlpha(1.0F);
+        findViewById(R.id.relative).setAlpha(1.0F);
         Animation anim = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-        findViewById(R.id.image_splash).startAnimation(anim);
+        findViewById(R.id.relative).startAnimation(anim);
     }
 }
